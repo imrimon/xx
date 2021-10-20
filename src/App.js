bde0fa96bb49795837647869ed562acf592f7811
imrimon <72396280+imrimon@users.noreply.github.com>
2021-10-20 21:25:10 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Header from './Pages/Shared/Header/Header';
-import Home from './Pages/Home/Home/Home';
-import NotFound from './Pages/NotFound/NotFound';
-import Footer from './Pages/Shared/Footer/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Notfound from './components/Notfound/Notfound';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home/Home';
+import Login from './components/Login/Login'
+import AuthProvider from './context/AuthProvider';
+import PrivateRouter from './PrivateRouter/PrivateRouter';
+import AboutUs from './components/AboutUs/AboutUs';
+import OurDoctors from './components/OurDoctors/OurDoctors';
+import ReadMore from './components/Home/Service/ReadMore/ReadMore';
+import Footer from './components/Footer/Footer';
+
 
 function App() {
+  
   return (
     <div className="App">
-      <Router>
-        <Header></Header>
-        <Switch>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-          <Route path="/home">
-            <Home></Home>
-          </Route>
-          <Route path="*">
-            <NotFound></NotFound>
-          </Route>
-        </Switch>
-        <Footer></Footer>
-      </Router>
+    <AuthProvider>
+      
+    <BrowserRouter>
+            <Header />
+             <Switch>
+               <Route exact path="/">
+                 <Home></Home>
+               </Route>
+               <Route  path="/home">
+                 <Home></Home>
+               </Route>
+               <Route path="/about">
+                 <AboutUs></AboutUs>
+               </Route>
+               <PrivateRouter path="/services/:serviceId">
+                 <ReadMore></ReadMore>
+               </PrivateRouter>
+               <Route path="/doctors">
+                 <OurDoctors></OurDoctors>
+               </Route>
+               <Route  path="/login">
+                  <Login></Login>
+                </Route>
+                 <Route path="*">
+                      <Notfound />
+                 </Route>
+             </Switch>
+             <Footer></Footer>
+         </BrowserRouter>
+    </AuthProvider>
+        
     </div>
   );
 }
